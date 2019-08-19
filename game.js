f319f13b7055700d07574749dc4cd0a36df37e1e
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-19 02:44:08 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
 var money = 0;
 function GatherMoney(money = money + 1;)
 document.getElementById("money").innerHTML = money;
+var money = 0;
+var Timer = window.setInterval(function(){Tick()}, 1000);
+
+function GatherMoney() {
+	money = money + 1;
+	document.getElementById("money").innerHTML = money;
+}
+
+function Tick() {
+	money++;
+	document.getElementById("money").innerHTML = money;
+}
