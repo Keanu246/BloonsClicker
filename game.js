a04832e76f826ed9966b3b675522e37b56505ae4
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-19 01:44:35 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,4 @@
 var money = 0;
 function GatherMoney()
 money = money + 1;
-<div class="label">Money</div>
-<div id="money" class="label">0</div>
 document.getElementById("money").innerHTML = money;
