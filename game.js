ec7dfb1eff9c856c6c72ba09f47a2866fbc2046b
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-19 02:56:40 +0100

    Update game.js

--- game.js
+++ game.js
@@ -18,3 +18,26 @@ var Building1Name = "Dart Monkey";
 var Building1Cost = 10;
 var Building1PerSec = 1;
 var Building1Qty = 0;
+
+if (money >= Building1Cost) {	
+money = money - Building1Cost;
+Building1Qty = Building1Qty + 1;
+document.getElementById("money").innerHTML = money;
+document.getElementById("Building1Qty").innerHTML = Building1Qty
+
+}
+
+function GameSave() {
+    this.money = 0;
+}
+
+window.onload = function() {
+    window.game = new GameSave();
+};
+
+function Building() {
+    this.name = "Dart Monkey";
+    this.cost = 10;
+    this.persec = 1;
+    this.qty = 0;
+}
