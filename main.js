3ad271d00e16ef120cd9db8ff0285fe5fc484404
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-28 00:38:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -255,4 +255,26 @@ var numberFormatters =
 		' novemdecillion',
 		' vigintillion'
 	])
-		
+};
+	
+function Beautify(value,floats)
+{
+	var negative=(value<0);
+	var decimal='';
+	var fixed=value.toFixed(floats);
+	if (value<1000000 && floats>0 && Math.floor(fixed)!=fixed) decimal='.'+(fixed.toString()).split('.')[1];
+	value=Math.floor(Math.abs(value));
+	if (floats>0 && fixed==value+1) value++;
+	var formatter=numberFormatters[Game.prefs.format?0:1];
+	var output=formatter(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g,',');
+	return negative?'-'+output:output+decimal;
+}
+
+var beautifyInTextFilter=/(([\d]+[,]*)+)/g;//new regex
+var a=/\d\d?\d?(?:,\d\d\d)*/g;//old regex
+function BeautifyInTextFunction(str){return Beautify(parseInt(str.replace(/,/g,''),10));};
+function BeautifyInText(str) {return str.replace(beautifyInTextFilter,BeautifyInTextFunction);}//reformat every number inside a string
+function BeautifyAll()//run through upgrades and achievements to reformat the numbers
+{
+	var func=function(what){what.desc=BeautifyInText(what.baseDesc);}
+}
