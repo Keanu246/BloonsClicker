8a0da03cc2e9659ab1b45ef519da9639a2741bfc
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2020-04-03 20:53:06 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -4,4 +4,10 @@ var formatBloons = function () {
     document.getElementById("clickCostDisplay").innerHTML = numberformat.format(clickCostDisplay)
     document.getElementById("dartMonkey").innerHTML = numberformat.format(dartPrice)
     document.getElementById("dartProduce").innerHTML = numberformat.format(dartBPS)
+    document.getElementById("tackShooter").innerHTML = numberformat.format(tackPrice)
+    document.getElementById("tackProduce").innerHTML = numberformat.format(tackBPS)
+    document.getElementById("sniperMonkey").innerHTML = numberformat.format(sniperPrice)
+    document.getElementById("sniperProduce").innerHTML = numberformat.format(sniperBPS)
+    document.getElementById("boomerangMonkey").innerHTML = numberformat.format(boomerangPrice)
+    document.getElementById("boomerangProduce").innerHTML = numberformat.format(boomerangBPS)
 }
