9bbc07b920b1fb77b1563ae54d07bc55fa791036
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2020-04-03 22:59:06 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,23 +1,13 @@
-var formatNumber = false
+var formatBloons = document.getElementById("bloons").innerHTML = numberformat.format(bloons);
+var formatNumber = false;
 
-function formatBloons(){
-   if(formatNumber){
+function formatValue(){
+   if(formatBloons){
       formatNumber = false;
-      document.getElementById("format").innerHTML = "Format";
+      document.getElementById("format").innerHTML = "Format!";
    }
    else{
       formatNumber = true;
       document.getElementById("format").innerHTML = "Formated!";
-   }
-}
-
-function formatValue(){
-   if(formatNumber){
-      document.getElementById("bloons").innerHTML = numberformat.format(bloons, {format: 'scientific'})
-      document.getElementById("bloonspersecond").innerHTML = numberformat.format(totalBPS, {format: 'scientific'})
-   }
-   else{
-      document.getElementById("bloons").innerHTML = numberformat.format(bloons);
-      document.getElementById("bloonspersecond").innerHTML = numberformat.format(totalBPS);
    }
 }
