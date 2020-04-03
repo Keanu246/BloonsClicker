ab2bf45dd08cf2d6640757e5b7970952aaff18f3
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2020-04-03 21:34:17 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,19 +1,10 @@
 function formatBloons(){
     if(formatNumber){
       formatNumber = true;
-      document.getElementById("format").innerHTML = "Format";
+      document.getElementById("format").innerHTML = "Formated!";
     }
     else if(formatNumber){
       document.getElementById("bloons").innerHTML = numberformat.format(bloons);
       document.getElementById("bloonspersecond").innerHTML = numberformat.format(totalBPS);
-      document.getElementById("clickCostDisplay").innerHTML = numberformat.format(clickCost);
-      document.getElementById("dartMonkey").innerHTML = numberformat.format(dartPrice);
-      document.getElementById("dartProduce").innerHTML = numberformat.format(dartBPS);
-      document.getElementById("tackShooter").innerHTML = numberformat.format(tackPrice);
-      document.getElementById("tackProduce").innerHTML = numberformat.format(tackBPS);
-      document.getElementById("sniperMonkey").innerHTML = numberformat.format(sniperPrice);
-      document.getElementById("sniperProduce").innerHTML = numberformat.format(sniperBPS);
-      document.getElementById("boomerangMonkey").innerHTML = numberformat.format(boomerangPrice);
-      document.getElementById("boomerangProduce").innerHTML = numberformat.format(boomerangBPS);
    }
 }
