b39047e19b685015d667ff530a8338df2f28ce9c
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2020-04-03 21:50:29 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -4,12 +4,10 @@ function formatBloons(){
    if(formatNumber){
       formatNumber = false;
       document.getElementById("format").innerHTML = "Format";
-      document.getElementById("bloons").innerHTML = numberformat.format(bloons, {format: 'scientific'})
    }
    else{
       formatNumber = true;
       document.getElementById("format").innerHTML = "Formated!";
-      document.getElementById("bloons").innerHTML = numberformat.format(bloons);
    }
 }
 
