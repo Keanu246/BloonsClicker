caaa152ce498b737680040ac7f229f92260890d2
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2020-04-03 21:31:07 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,13 +1,18 @@
-function formatBloons() {
-    document.getElementById("bloons").innerHTML = numberformat.format(bloons);
-    document.getElementById("bloonspersecond").innerHTML = numberformat.format(totalBPS);
-    document.getElementById("clickCostDisplay").innerHTML = numberformat.format(clickCost);
-    document.getElementById("dartMonkey").innerHTML = numberformat.format(dartPrice);
-    document.getElementById("dartProduce").innerHTML = numberformat.format(dartBPS);
-    document.getElementById("tackShooter").innerHTML = numberformat.format(tackPrice);
-    document.getElementById("tackProduce").innerHTML = numberformat.format(tackBPS);
-    document.getElementById("sniperMonkey").innerHTML = numberformat.format(sniperPrice);
-    document.getElementById("sniperProduce").innerHTML = numberformat.format(sniperBPS);
-    document.getElementById("boomerangMonkey").innerHTML = numberformat.format(boomerangPrice);
-    document.getElementById("boomerangProduce").innerHTML = numberformat.format(boomerangBPS);
+function formatBloons(){
+    if(formatNumber){
+      formatNumber = true;
+      document.getElementById("format").innerHTML = "Format";
+    }
+    else if(formatNumber){
+      document.getElementById("bloons").innerHTML = numberformat.format(bloons);
+      document.getElementById("clickCostDisplay").innerHTML = numberformat.format(clickCost);
+      document.getElementById("dartMonkey").innerHTML = numberformat.format(dartPrice);
+      document.getElementById("dartProduce").innerHTML = numberformat.format(dartBPS);
+      document.getElementById("tackShooter").innerHTML = numberformat.format(tackPrice);
+      document.getElementById("tackProduce").innerHTML = numberformat.format(tackBPS);
+      document.getElementById("sniperMonkey").innerHTML = numberformat.format(sniperPrice);
+      document.getElementById("sniperProduce").innerHTML = numberformat.format(sniperBPS);
+      document.getElementById("boomerangMonkey").innerHTML = numberformat.format(boomerangPrice);
+      document.getElementById("boomerangProduce").innerHTML = numberformat.format(boomerangBPS);
+   }
 }
